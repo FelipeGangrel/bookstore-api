ccb4ec76a5b531917788dbb5b8b3125145850c73
Felipe Gangrel <felipefrog@gmail.com>
2023-11-15 14:50:53 -0300

    Updates users migration

--- database/migrations/1700067817456_users.ts
+++ database/migrations/1700067817456_users.ts
@@ -1,4 +1,5 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+import { UserRole } from 'App/Contracts/Users'
 
 export default class extends BaseSchema {
   protected tableName = 'users'
@@ -7,7 +8,10 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
       table.string('email', 255).notNullable().unique()
+      table.string('name', 255).notNullable()
+      table.enum('role', Object.values(UserRole)).defaultTo(UserRole.CLIENT).notNullable()
       table.string('password', 180).notNullable()
+      table.string('password_reset_token', 10).nullable()
       table.string('remember_me_token').nullable()
 
       /**
@@ -15,6 +19,7 @@ export default class extends BaseSchema {
        */
       table.timestamp('created_at', { useTz: true }).notNullable()
       table.timestamp('updated_at', { useTz: true }).notNullable()
+      table.timestamp('deleted_at', { useTz: true }).nullable()
     })
   }
 

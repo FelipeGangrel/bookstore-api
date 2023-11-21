43d993a329770c281eb25e91337a4dea2fe0db4c
Felipe Gangrel <felipefrog@gmail.com>
2023-11-21 16:11:26 -0300

    Creates validation exception reporter

---
+++ app/Exceptions/ValidationExceptionReporter.ts
@@ -0,0 +1,62 @@
+import type { ErrorReporterContract, MessagesBagContract } from '@ioc:Adonis/Core/Validator'
+import { ValidationException } from '@ioc:Adonis/Core/Validator'
+
+type ValidationError = {
+  message: string
+  field: string
+}
+
+type ErrorBody = {
+  message: string
+  validationErrors: ValidationError[]
+  code: string
+}
+
+export default class ValidationExceptionReporter implements ErrorReporterContract<ErrorBody> {
+  public hasErrors: boolean
+  private validationErrors: ValidationError[] = []
+
+  constructor(
+    private messages: MessagesBagContract,
+    private bail: boolean
+  ) {}
+
+  public report(
+    pointer: string,
+    rule: string,
+    message?: string | undefined,
+    arrayExpressionPointer?: string | undefined,
+    args?: any
+  ): void {
+    this.hasErrors = true
+
+    const errorMessage = this.messages.get(
+      pointer,
+      rule,
+      message || `${pointer} field is invalid`,
+      arrayExpressionPointer,
+      args
+    )
+
+    this.validationErrors.push({
+      message: errorMessage,
+      field: pointer,
+    })
+
+    if (this.bail) {
+      throw new this.toError()
+    }
+  }
+
+  public toError() {
+    throw new ValidationException(false, this.toJSON())
+  }
+
+  public toJSON(): ErrorBody {
+    return {
+      message: 'Your form has validation errors',
+      validationErrors: this.validationErrors,
+      code: 'E_VALIDATION_FAILURE',
+    }
+  }
+}

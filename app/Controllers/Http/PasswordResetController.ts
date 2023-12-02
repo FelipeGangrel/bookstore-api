e8d7308ec63003d7be8eabf0f5f23f09f29fccf4
Felipe Gangrel <felipefrog@gmail.com>
2023-12-02 14:41:16 -0300

    Add password reset step 2 validation and reset password functionality

--- app/Controllers/Http/PasswordResetController.ts
+++ app/Controllers/Http/PasswordResetController.ts
@@ -1,6 +1,7 @@
 import type { HttpContextContract } from 'App/Contracts/Common'
 import PasswordResetService from 'App/Services/PasswordResetService'
 import PasswordResetStep1Validator from 'App/Validators/Auth/PasswordResetStep1Validator'
+import PasswordResetStep2Validator from 'App/Validators/Auth/PasswordResetStep2Validator'
 
 export default class PasswordResetController {
   private readonly passwordResetService = new PasswordResetService()
@@ -8,5 +9,10 @@ export default class PasswordResetController {
   public async generateToken(ctx: HttpContextContract) {
     const data = await ctx.request.validate(PasswordResetStep1Validator)
     return this.passwordResetService.generateToken(data)
+  }
+
+  public async resetPassword(ctx: HttpContextContract) {
+    const data = await ctx.request.validate(PasswordResetStep2Validator)
+    return this.passwordResetService.resetPassword(data)
   }
 }

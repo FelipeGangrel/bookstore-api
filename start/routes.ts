ff47b737521e843ee341e7806e1369086711317e
Felipe Gangrel <felipefrog@gmail.com>
2023-12-02 20:09:24 -0300

    Add user routes

--- start/routes.ts
+++ start/routes.ts
@@ -1,6 +1,7 @@
 import Route from '@ioc:Adonis/Core/Route'
 import 'App/Routes/AuthRoutes'
 import 'App/Routes/PasswordResetRoutes'
+import 'App/Routes/UserRoutes'
 
 Route.get('/', async () => {
   return { hello: 'world' }

6b4ea486e429bbaeac479f4a2d0e026d3780de09
Felipe Gangrel <felipefrog@gmail.com>
2023-12-03 10:50:05 -0300

    Refactor password reset controller validators

--- app/Controllers/Http/PasswordResetController.ts
+++ app/Controllers/Http/PasswordResetController.ts
@@ -1,19 +1,19 @@
 import type { HttpContextContract } from 'App/Contracts/Common'
 import PasswordResetService from 'App/Services/PasswordResetService'
-import PasswordResetStep1Validator from 'App/Validators/Auth/PasswordResetStep1Validator'
-import PasswordResetStep2Validator from 'App/Validators/Auth/PasswordResetStep2Validator'
+import GenerateTokenValidator from 'App/Validators/PasswordReset/GenerateTokenValidator'
+import UpdatePasswordValidator from 'App/Validators/PasswordReset/UpdatePasswordValidator'
 
 export default class PasswordResetController {
   private readonly passwordResetService = new PasswordResetService()
 
   public async generateToken(ctx: HttpContextContract) {
-    const data = await ctx.request.validate(PasswordResetStep1Validator)
+    const data = await ctx.request.validate(GenerateTokenValidator)
     await this.passwordResetService.generateToken(data)
     return ctx.response.json({ message: 'Código para recuperação de senha gerado com sucesso' })
   }
 
   public async updatePassword(ctx: HttpContextContract) {
-    const data = await ctx.request.validate(PasswordResetStep2Validator)
+    const data = await ctx.request.validate(UpdatePasswordValidator)
     await this.passwordResetService.updatePassword(data)
     return ctx.response.json({ message: 'senha atualizada com sucesso' })
   }

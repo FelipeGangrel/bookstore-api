ac9ee3c8d8ae6d0cdbea92e75307271ccccf16af
Felipe Gangrel <felipefrog@gmail.com>
2023-12-03 12:07:47 -0300

    Add genre schema types

--- app/Contracts/Genre.ts
+++ app/Contracts/Genre.ts
@@ -1,3 +1,7 @@
-import { PropsWithFindMany } from './Common'
+import { ExtractSchemaProps, PropsWithFindMany } from './Common'
+import CreateGenreValidator from 'App/Validators/Genre/CreateGenreValidator'
+import UpdateGenreValidator from 'App/Validators/Genre/UpdateGenreValidator'
 
 export type FindGenres = PropsWithFindMany
+export type CreateGenreSchema = ExtractSchemaProps<CreateGenreValidator>
+export type UpdateGenreSchema = ExtractSchemaProps<UpdateGenreValidator>

487e5e71be660a8be4bcf30bb05f4e36919360b5
Felipe Gangrel <felipefrog@gmail.com>
2023-12-03 09:11:54 -0300

    Refactor ProductFactory to use string.capitalCase for product names

--- database/factories/ProductFactory.ts
+++ database/factories/ProductFactory.ts
@@ -1,9 +1,10 @@
 import Product from 'App/Models/Product'
 import Factory from '@ioc:Adonis/Lucid/Factory'
+import { string } from '@ioc:Adonis/Core/Helpers'
 
 export default Factory.define(Product, ({ faker }) => {
   return {
-    name: faker.lorem.words(faker.number.int({ min: 1, max: 3 })),
+    name: string.capitalCase(faker.lorem.words(faker.number.int({ min: 1, max: 4 }))),
     description: faker.lorem.paragraph(),
     price: faker.number.float({ min: 10, max: 400, precision: 2 }),
   }

95c11adc04d6fa5fcb27c99e8fecb3b5d1d80241
Felipe Gangrel <felipefrog@gmail.com>
2023-12-03 10:50:42 -0300

    Refactor user service

--- app/Services/UserService.ts
+++ app/Services/UserService.ts
@@ -1,9 +1,8 @@
-import { UserRole } from 'App/Contracts/Users'
+import { RegisterUserSchema, UserRole } from 'App/Contracts/Users'
 import User from 'App/Models/User'
-import { RegisterUserValidatorSchema } from 'App/Validators/User/RegisterUserValidator'
 
 export default class UserService {
-  public async registerUser(payload: RegisterUserValidatorSchema, role?: UserRole): Promise<User> {
+  public async registerUser(payload: RegisterUserSchema, role?: UserRole): Promise<User> {
     const user = await User.create({
       name: payload.name,
       email: payload.email,

85952d67d525156e96ff412e6c9ddb89a162b653
Felipe Gangrel <felipefrog@gmail.com>
2023-12-03 11:43:48 -0300

    Add GenreService with CRUD operations

---
+++ app/Services/GenreService.ts
@@ -0,0 +1,40 @@
+import { PaginatedData } from 'App/Contracts/Common'
+import { FindGenres } from 'App/Contracts/Genre'
+import Genre from 'App/Models/Genre'
+
+export default class GenreService {
+  public async getGenres(data: FindGenres): Promise<PaginatedData> {
+    const { page = 1, limit = 10, orderBy = 'name', direction = 'asc', search } = data
+
+    const query = Genre.query()
+
+    if (search) {
+      query.where('name', 'like', `%${search}%`)
+    }
+
+    query.orderBy(orderBy, direction)
+
+    const genres = await query.paginate(page, limit)
+    return genres.serialize()
+  }
+
+  public async getGenre(id: number) {
+    return await Genre.findOrFail(id)
+  }
+
+  public async createGenre(data: Genre) {
+    return await Genre.create(data)
+  }
+
+  public async updateGenre(id: number, data: Partial<Genre>) {
+    const genre = await Genre.findOrFail(id)
+    genre.merge(data)
+    await genre.save()
+    return genre
+  }
+
+  public async deleteGenre(id: number) {
+    const genre = await Genre.findOrFail(id)
+    await genre.delete()
+  }
+}

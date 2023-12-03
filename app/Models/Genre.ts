3e56b9a001af409510bf47575a857dd21d2ae7fd
Felipe Gangrel <felipefrog@gmail.com>
2023-12-03 10:14:05 -0300

    Add hasManyThrough relationship for products in Genre model

--- app/Models/Genre.ts
+++ app/Models/Genre.ts
@@ -1,5 +1,7 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, HasManyThrough, column, hasManyThrough } from '@ioc:Adonis/Lucid/Orm'
+import Product from './Product'
+import ProductGenre from './ProductGenre'
 
 export default class Genre extends BaseModel {
   @column({ isPrimary: true })
@@ -13,4 +15,13 @@ export default class Genre extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  /** Relationships */
+
+  @hasManyThrough([() => Product, () => ProductGenre], {
+    onQuery(query) {
+      query.whereNull('deletedAt')
+    },
+  })
+  public products: HasManyThrough<typeof Product>
 }

d99cd68eddc2d2b72b2f603b0ba9fe2e87ab78cd
Felipe Gangrel <felipefrog@gmail.com>
2023-12-03 09:02:46 -0300

    Update seeders for category and genre

--- database/seeders/Category.ts
+++ database/seeders/Category.ts
@@ -2,9 +2,18 @@ import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import Category from 'App/Models/Category'
 import CategoryFactory from 'Database/factories/CategoryFactory'
 
+const categories: Partial<Category>[] = [
+  {
+    name: 'Livros',
+  },
+  {
+    name: 'Papelaria',
+  },
+]
+
 export default class extends BaseSeeder {
   public async run() {
     await Category.truncate()
-    await CategoryFactory.createMany(10)
+    await CategoryFactory.merge(categories)
   }
 }
--- database/seeders/Genre.ts
+++ database/seeders/Genre.ts
@@ -2,9 +2,21 @@ import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import Genre from 'App/Models/Genre'
 import GenreFactory from 'Database/factories/GenreFactory'
 
+const genres: Partial<Genre>[] = [
+  {
+    name: 'Romance histórico',
+  },
+  {
+    name: 'Ficção científica',
+  },
+  {
+    name: 'Fantasia',
+  },
+]
+
 export default class extends BaseSeeder {
   public async run() {
     await Genre.truncate()
-    await GenreFactory.createMany(10)
+    await GenreFactory.merge(genres)
   }
 }
